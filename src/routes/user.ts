126cb06aa6e0729ef98400d53d596e7ff4c3cc68
Hongsungj <hsj121502@naver.com>
2024-09-22 22:14:49 +0900

    user 라우터에 회원 정보 조회, 회원 가입 샘플 코드 추가

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,40 @@
-import { NextFunction, Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express';
+import { db } from '../database';
 
 export const userPath = '/user'
 export const userRouter = Router()
+
+/**
+ * 회원 정보 조회
+ */
+userRouter.get(
+  '/',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { kakaoId } = req.body
+
+      console.log('회원 정보 조회', kakaoId)
+
+      if(!kakaoId) {
+        throw new Error('kakaoId는 필수 전달 값입니다.')
+      }
+
+      const users = db.ref('users')
+      const userSnapShot = await users.orderByChild('kakaoId').equalTo(kakaoId).once('value');
+
+      if(userSnapShot.exists()) {
+        const user = userSnapShot.val()
+
+        res.json({user})
+      } else {
+        throw new Error('not found user')
+      }
+    }
+    catch(err) {
+      next(err)
+    }
+  },
+)
 
 /**
  * 로그인 OR 회원 가입
@@ -11,9 +44,36 @@ export const userRouter = Router()
  */
 userRouter.post(
   '/',
-  async (req: Request, res: Response, _next: NextFunction) => {
-    console.log('로그인 및 회원 가입', req.body)
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log('로그인 및 회원 가입', req.body)
 
-    res.json({message: 'OK'})
-},
+      const users = db.ref('users')
+
+      const kakaoId = 'sample-kakao-id'
+
+      // 회원 정보 조회
+      const userSnapShot = await users.orderByChild('kakaoId').equalTo(kakaoId).once('value');
+
+      if(userSnapShot.exists()) {
+        const user = userSnapShot.val()
+
+        console.log('회원 정보 찾음!', user)
+      } else {
+        console.log(`${kakaoId}에 해당하는 회원을 찾을 수 없음`)
+      }
+    
+      // 회원 가입 처리
+      // const newUser = users.push()
+      // await newUser.set({
+      //   kakaoId: 'sample-kakao-id',
+      //   nickname: 'sample-nickname',
+      //   phone: 'sample-phone'
+      // });
+
+      res.json({message: 'OK'}) 
+    }catch(err) {
+      next(err)
+    }
+  }
 )
